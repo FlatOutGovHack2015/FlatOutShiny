70b18c16f9191e949325f9b2376e2356db9267a1
Chris Hansen <cmhhansen@outlook.com>
2015-07-04 18:35:22 +1200

    add readme.

--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 shinyServer(function(input, output, session){
 
-   sessionvars <- reactiveValues(profile="")
+   sessionvars <- reactiveValues(profile="", page="1")
 
    topPage <- list(
       div(style="display:block; padding: 5px;",
@@ -12,6 +12,34 @@ shinyServer(function(input, output, session){
       div(style="display:block; padding: 5px;",
          actionButton("custombutton", "Custom", width=305))
    )
+
+   locations <- list(
+      div(actionButton("next1", "next"))
+   )
+
+   choices <- reactive({
+      selected <- c()
+      if (sessionvars$profile=="singleparent") selected <- c(1,2,4,7,9)
+      else if (sessionvars$profile=="youngprofessional") selected <- c(2,4,5,8,9)
+      else if (sessionvars$profile=="student") selected <- c(3,4,6,9,10,12)
+      else if (sessionvars$profile=="family") selected <- c(3,4,6,7,8)
+      else if (sessionvars$profile=="custom") selected <- c()
+      list(h2(sessionvars$profile), checkboxGroupInput("choices", "",
+         choices=list("Work"=1, "Supermarket"=2, "Sports clubs"=3,
+            "School (specific)"=4, "Transport"=5, "Parking"=6, "Gym"=7,
+            "Farmer's market"=8, "Daycare (for children)"=9, "Church"=10,
+            "Safety (Low crime-rate)"=11), selected
+      ), div(actionButton("next2", "next"), style="display: inline-block; float: right; "))
+   })
+
+   output$main <- renderUI({
+      if(sessionvars$profile=="") return(topPage)
+      return(locations)
+   })
+
+   observeEvent(input$back, {
+
+   })
 
    observeEvent(input$singleparentbutton, {
       sessionvars$profile <- "singleparent"
@@ -33,25 +61,7 @@ shinyServer(function(input, output, session){
       sessionvars$profile <- "custom"
    })
 
-   locations <- list()
-
-   choices <- reactive({
-      selected <- c()
-      if (sessionvars$profile=="singleparent") selected <- c(1,2,4,7,9)
-      else if (sessionvars$profile=="youngprofessional") selected <- c(2,4,5,8,9)
-      else if (sessionvars$profile=="student") selected <- c(3,4,6,9,10,12)
-      else if (sessionvars$profile=="family") selected <- c(3,4,6,7,8)
-      else if (sessionvars$profile=="custom") selected <- c()
-      list(h2(sessionvars$profile), checkboxGroupInput("choices", "",
-         choices=list("Work"=1, "Supermarket"=2, "Sports clubs"=3,
-            "School (specific)"=4, "Transport"=5, "Parking"=6, "Gym"=7,
-            "Farmer's market"=8, "Daycare (for children)"=9, "Church"=10,
-            "Safety (Low crime-rate)"=11), selected
-      ), div(actionButton("next1", "next"), style="display: inline-block; float: right; "))
-   })
-
-   output$main <- renderUI({
-      if(sessionvars$profile=="") return(topPage)
-      return(choices())
+   observeEvent(input$next1, {
+      output$main <- renderUI(choices())
    })
 })
